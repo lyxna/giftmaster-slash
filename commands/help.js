a8f1f177098b22bc2172c511e50db9c89819fa33
Lyxn <115940004+lyxna@users.noreply.github.com>
2024-04-28 20:30:43 +0200

    help.js

--- commands/help.js
+++ commands/help.js
@@ -4,27 +4,27 @@ const config = require('../config.json');
 const stock = require('./stock');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('help')
-		.setDescription('Display the command list.'),
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Display the command list.'),
 
-	async execute(interaction) {
-		const { commands } = interaction.client;
+    async execute(interaction) {
+        const { commands } = interaction.client;
 
-		const commandListEmbed = new MessageEmbed()
-			.setColor(config.color.default)
-			.setTitle('Help Panel')
-			.setDescription(`👋 Hello and welcome to **${interaction.guild.name}**! 🌟 We are here to provide you with the best services. 🚀`)
-			.setImage(config.banner)
-			.setThumbnail(interaction.client.user.displayAvatarURL({ dynamic: true, size: 64 })) // Set the bot's avatar as the thumbnail
-			.addFields({
-				name: `Commands`,
-				value: "`/help`   **Displays the help command**\n`/create` **Create a new service**\n`/free`   **Generate a reward**\n`/add`    **Add a reward to the stock**\n`/stock`  **View the current stock**\n`/premium` **Generate premium reward**"
-			})
-			.setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
-			.setTimestamp()
-			.addField('Useful Links', `[**Website**](${config.website}) [**Discord**](https://dsc.gg/sciencegear)`);
+        const commandListEmbed = new MessageEmbed()
+            .setColor(config.color.default)
+            .setTitle('Help Panel')
+            .setDescription(`👋 Welcome to **${interaction.guild.name}**! 🌟 We are here to provide you with the best services. 🚀`)
+            .setImage(config.banner)
+            .setThumbnail(interaction.client.user.displayAvatarURL({ dynamic: true, size: 64 })) // Set the bot's avatar as the thumbnail
+            .addFields({
+                name: `Commands`,
+                value: "`/help` **Create a new service**\n`/free` **Add a reward to the stock**\n`/stock`  **View the current stock**\n`/premium` **Generate premium reward**"
+            })
+            .setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
+            .setTimestamp()
+            .addField('Useful Links', `[**Website**](${config.website}) [**Discord**](https://discord.gg/nbaim)`);
 
-		await interaction.reply({ embeds: [commandListEmbed] });
-	},
+        await interaction.reply({ embeds: [commandListEmbed] });
+    },
 };
